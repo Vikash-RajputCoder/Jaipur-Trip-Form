84d4ce9b2c16bb35127db0124d8ce73bded4ade0
Vikash Rajput <rajputvikash2006@gmail.com>
2025-04-02 13:27:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Travel Form</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <img class="bg" src="Bgg.jpg" alt="Agra College">
+    <div class="container">
+        <h1>Welcome To Agra College, Jaipur Trip</h1>
+        <p>Enter your details and submit this form to confirm your participation in the trip</p>
+        
+        <?php
+        if($insert == true){
+        echo "<p class='submitMsg'>Thank you for submitting your form. We are happy to see you joining us for the jaipur trip.</p>";
+        }
+        ?>
+
+        <form action="index.php" method="post">
+            <input type="text" name="name" id="name" placeholder="Enter your name">
+            <input type="text" name="age" id="age" placeholder="Enter your age">
+            <input type="text" name="gender" id="gender" placeholder="Enter your gender">
+            <input type="email" name="email" id="email" placeholder="Enter your email">
+            <input type="tel" name="phone" id="phone" placeholder="Enter your phone">
+            <textarea name="decs" id="decs" cols="30" rows="10" placeholder="Enter your any other information"></textarea>
+            <button class="btn" type="Submit">Submit</button>
+        </form>
+    </div>
+</body>
+</html>
 <?php
 $insert = false;
 if (isset($_POST['name'])) {
@@ -31,38 +63,3 @@ if (isset($_POST['name'])) {
     $con->close();
 }
 ?>
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to Travel Form</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <img class="bg" src="Bgg.jpg" alt="Agra College">
-    <div class="container">
-        <h1>Welcome To Agra College, Jaipur Trip</h1>
-        <p>Enter your details and submit this form to confirm your participation in the trip</p>
-        
-        <?php
-        if($insert == true){
-        echo "<p class='submitMsg'>Thank you for submitting your form. We are happy to see you joining us for the jaipur trip.</p>";
-        }
-        ?>
-
-        <form action="index.php" method="post">
-            <input type="text" name="name" id="name" placeholder="Enter your name">
-            <input type="text" name="age" id="age" placeholder="Enter your age">
-            <input type="text" name="gender" id="gender" placeholder="Enter your gender">
-            <input type="email" name="email" id="email" placeholder="Enter your email">
-            <input type="tel" name="phone" id="phone" placeholder="Enter your phone">
-            <textarea name="decs" id="decs" cols="30" rows="10" placeholder="Enter your any other information"></textarea>
-            <button class="btn" type="Submit">Submit</button>
-        </form>
-    </div>
-</body>
-</html>
